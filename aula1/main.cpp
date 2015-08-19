fe96ed2856bcb967ecf93b0a8bc7b0ff0747d4cb
carlosqueiroz <carlosqueiroz@users.noreply.github.com>
2015-08-19 09:18:21 -0300

    Created by Coding Ground Client

---
+++ aula1/main.cpp
@@ -0,0 +1,34 @@
+// aula 1 sistemas embarcados - relembrando C++
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     /*Operador condicional ( ? ) 
+     //  7==5 ? 4 : 3 // retorna 3 uma vez que 7 é diferente de 5
+     
+     // Conversão de dados (type casting) 
+     int i;
+     float f = 3.14;
+     i = (int) f; // posso converter das duas maneiras
+     i = int ( f ); 
+     */
+     //a = sizeof (char); Retorna o número de bytes do tipo ou objeto
+    
+    // if else
+    int x; 
+    x=10;
+    if (x == 100) {
+     cout << "x is ";
+     cout << x;
+    } else {
+    cout << "não é 100" << endl; 
+    
+    }
+    
+   
+   
+   return 0;
+}
+
