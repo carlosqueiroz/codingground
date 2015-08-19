27e9ede9743d1ee584163acca65232381ae3dff9
carlosqueiroz <carlosqueiroz@users.noreply.github.com>
2015-08-19 11:00:18 -0300

    Update main.cpp

--- sobrecargaFuncoes/main.cpp
+++ sobrecargaFuncoes/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-
+/*
+É possível declarar variáveis locais e globais com o mesmo
+nome dentro de um bloco. Para acessar uma variável global
+quando uma variával local com o mesmo nome está no escopo,
+usa-se o operador :: (resolução de escopo).
+*/
 using namespace std;
-
+int ref = 1000; // variavel global
 int main()
 {
-   cout << "Hello World" << endl; 
+   int j = 0;
+ if (j == 0)
+ {
+ int ref = 2; // variavel local
+ cout << ref*::ref << endl; // calculando variavel local com global de mesmo nome usando ::
+ }
    
    return 0;
 }
