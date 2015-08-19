8d65f33de2b8ba28db3329e8333cbebf9ed8fd9a
carlosqueiroz <carlosqueiroz@users.noreply.github.com>
2015-08-19 11:05:33 -0300

    Update main.cpp

--- sobrecargaFuncoes/main.cpp
+++ sobrecargaFuncoes/main.cpp
@@ -7,6 +7,16 @@ usa-se o operador :: (resolução de escopo).
 */
 using namespace std;
 int ref = 1000; // variavel global
+
+// funcoes
+int quadrado(int x){
+ return x * x;
+}
+double quadrado (double y){
+ return y * y;
+} 
+
+
 int main()
 {
    int j = 0;
@@ -15,6 +25,14 @@ int main()
  int ref = 2; // variavel local
  cout << ref*::ref << endl; // calculando variavel local com global de mesmo nome usando ::
  }
+ 
+ // sobrecarga de funcoes
+ int r1; double f1;
+ r1 = quadrado( 7 );
+ f1 = quadrado ( 2.2 );
+ cout << “r1 = ” << r1 << “, f1 = ” << f1 << endl; 
+ 
+ 
    
    return 0;
 }
